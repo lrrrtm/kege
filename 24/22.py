2031aea6188890094110dde6a0697859908b1bd0
lrrrtm <lrrrtm.edu@yandex.ru>
2022-04-17 22:34:21 +0200

    checked

--- 24/22.py
+++ 24/22.py
@@ -1,4 +1,4 @@
-with open("source/24.22 .txt") as f:
+with open("source/24.22.txt") as f:
     data = f.readline()
 
 count = maxi = 0
@@ -7,8 +7,7 @@ for a in range(len(data) - 1):
     if data[a] == data[a + 1]:
         count += 1
     else:
-        if count > maxi:
-            maxi = count
+        maxi = max(maxi, count)
         count = 0
 
 for a in range(len(data) - 1):
@@ -18,3 +17,5 @@ for a in range(len(data) - 1):
         if count == maxi:
             print(data[a], maxi)
             break
+
+#ans: 92
