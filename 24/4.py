7999d0ae1a29f6c6d7cdc7eb2757b26c6bf667e8
lrrrtm <lrrrtm.edu@yandex.ru>
2022-04-17 20:11:44 +0200

    update

--- 24/4.py
+++ 24/4.py
@@ -2,16 +2,13 @@ with open("source/24.4.txt") as f:
     data = f.readline()
 
 start = end = 0
+maxi = 0
 
 for a in range(1, len(data) - 1):
     if ord(data[a]) > ord(data[a - 1]) and ord(data[a]) > ord(data[a + 1]):
-        start = a
-        break
+        start, end = end, a
+        maxi = max(maxi, (end - start))
 
-data = data[::-1]
-for a in range(1, len(data) - 1):
-    if ord(data[a]) > ord(data[a - 1]) and ord(data[a]) > ord(data[a + 1]):
-        end = len(data) - 1 - a
-        break
 
-print(end-start)
+
+print(maxi)
