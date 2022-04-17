0d490d038ea94c6d26b5a03302deec9be7bc72ad
lrrrtm <lrrrtm.edu@yandex.ru>
2022-04-17 12:32:58 +0200

    4-5

--- 24/5.py
+++ 24/5.py
@@ -7,7 +7,7 @@ for a in range(len(data) - 1):
     if data[a] == 'B':
         dict[data[a+1]] += 1
 
-4-5maxi, chr = 0, ""
+maxi, chr = 0, ""
 for a in "QAZWSXEDCRFVTGBYHNUJMIKOLP":
     if dict[a] > maxi:
         maxi = dict[a]
